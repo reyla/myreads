e059c43d5bd1c205d1a3d362f256be6adeb44989
Kate Speed <katespeed@gmail.com>
2018-09-04 20:01:43 -0400

    Change Add Book link to Router command

--- src/Shelves.js
+++ src/Shelves.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 class Shelves extends React.Component {
     render() {
@@ -159,7 +160,7 @@ class Shelves extends React.Component {
                 </div>
                 </div>
                 <div className="open-search">
-                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    <Link to="/search">Add a book</Link>
                 </div>
           </div>
         )
