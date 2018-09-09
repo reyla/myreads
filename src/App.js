d4bea2e13c04595700a55c65788ce33e47f245b1
Kate Speed <katespeed@gmail.com>
2018-09-08 21:11:13 -0400

    Testing shelves

--- src/App.js
+++ src/App.js
@@ -17,15 +17,43 @@ class BooksApp extends React.Component {
     })
   }
 
+  updateShelf(book, shelf) {
+    BooksAPI.update(book, shelf).then(
+      console.log(this.state.books)
+    )
+  }
+
   render() {
+    
     return (
       <div className="app">
         <Route path="/search" component={Search}/>
         <Route exact path="/" render={() => (
-                <Shelves
-                  books={this.state.books}
-                />
-            )}/>
+                <div className="list-books">
+                  <div className="list-books-title">
+                      <h1>MyReads</h1>
+                  </div>
+                  <div className="list-books-content">
+                      <div>
+                      <Shelves 
+                        shelfTitle='Currently Reading' 
+                        currentShelf={this.state.books.filter((book) => {return book.shelf === 'currentlyReading'})} 
+                        onUpdateShelf={this.updateShelf} />
+                      <Shelves 
+                        shelfTitle='Read' 
+                        currentShelf={this.state.books.filter((book) => {return book.shelf === 'read'})} 
+                        onUpdateShelf={this.updateShelf} />
+                      <Shelves 
+                        shelfTitle='Want To Read' 
+                        currentShelf={this.state.books.filter((book) => {return book.shelf === 'wantToRead'})} 
+                        onUpdateShelf={this.updateShelf} />
+                      <Shelves
+                        books={this.state.books}
+                        onUpdateShelf={this.updateShelf} />
+                      </div>
+                  </div>
+                </div>
+                  )}/>
       </div>
     )
   }
--- src/Shelves.js
+++ src/Shelves.js
@@ -3,69 +3,46 @@ import { Link } from 'react-router-dom'
 import BooksApp from './App';
 
 class Shelves extends React.Component {
-    
-
-    
+ 
     render() {
-        const { books, onUpdateShelf } = this.props
+        const { books, 
+                onUpdateShelf,
+         } = this.props
         console.log(books)
+
         return (          
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                {books.map((book) => (
-                                
-                                <li key={book.id}>
-                                    <div className="book">
-                                        <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                            <div className="book-shelf-changer">
-                                            <select>
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                            </div>
-                                        </div>
-                                        <div className="book-title">{book.title}</div>
-                                        <div className="book-authors">{book.authors}</div>
+        <div>
+            {books.map((book) => (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">shelfTitle</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">                                
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                    <select value={this.props.book.shelf} onChange={(event) => {this.props.updateShelf(this.props.book, event)}}>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading" onChange={() => onUpdateShelf(book, "currentlyReading")}>Currently Reading</option>
+                                        <option value="wantToRead" onChange={() => onUpdateShelf(book, "wantToRead")}>Want to Read</option>
+                                        <option value="read" onChange={() => onUpdateShelf(book, "read")}>Read</option>
+                                        <option value="none" onChange={() => onUpdateShelf(book, "")}>None</option>
+                                    </select>
                                     </div>
-                                </li>
-                                ))}
-                                </ol>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors}</div>
                             </div>
-                        </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Want to Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                
-                                </ol>
-                            </div>
-                        </div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Read</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                               
-                                </ol>
-                            </div>
-                        </div>
-                    </div>
+                        </li>
+                    </ol>
                 </div>
-                <div className="open-search">
-                    <Link to="/search">Add a book</Link>
-                </div>
-          </div>
+            </div>
+             ))}        
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+        </div>
         )
 
     }
