debe07155cefa360545e04c3a461a7ad20b22bca
Kate Speed <katespeed@gmail.com>
2018-09-16 18:08:10 -0400

    Sort books on shelves by Title

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class BooksApp extends React.Component {
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
+      books.sort(sortBy('title'))
       this.setState({ books })
     })
   }
