6c7c7e816150aafbcaf054dfacbd6871e39af639
Kate Speed <katespeed@gmail.com>
2018-09-17 19:17:35 -0400

    Add comma when there are multiple authors

--- src/Book.js
+++ src/Book.js
@@ -26,7 +26,7 @@ class Book extends React.Component {
                     </div>
                 </div>
                 <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                <div className="book-authors">{this.props.book.authors ? this.props.book.authors.join(', ') : ''}</div>
             </div>
         )
     }
